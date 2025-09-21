a9b19a9fc889c295035a6c03a143d9c594e06fe3
mintak21 <mintak21@gmail.com>
2025-09-21 16:56:09 +0900

    add: setup file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> $HOME/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
